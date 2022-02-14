686ba1cb46e4d2504370e69228f66a106491394e
kmo6bf <kmo6bf@gmail.com>
2022-02-14 15:29:49 +0900

    ToDo 기능 구현

--- js/background.js
+++ js/background.js
@@ -2,6 +2,5 @@ const randomImgNumber = Math.floor(Math.random() * 8);
 const bgImg = document.createElement("img");
 
 bgImg.src = `../resource/img/${randomImgNumber}.jpg`;
-console.log(bgImg);
 
 document.body.appendChild(bgImg);
--- js/greetings.js
+++ js/greetings.js
@@ -5,22 +5,16 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = "username";
 
-// 사용자가 입력한 이름이 없으면 입력창을 출력
-// 존재하면 greeting 출력!
-// printInputOrGretting();
-
 function onLoginSubmit(e) {
-    const username = loginInput.value;
-
     e.preventDefault();
-
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    paintGreeting(username)
+    paintGreetings(username);
 }
 
-function paintGreeting(username) {
-    greeting.innerText = `Hello! ${savedUsername}!`;
+function paintGreetings(username) {
+    greeting.innerText = `Hello! ${username}!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -30,6 +24,5 @@ if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreeting(savedUsername)
+    paintGreetings(savedUsername)
 }
-
